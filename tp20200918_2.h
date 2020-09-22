8213213a473b2d4470a65c9b87b046114e5cf573
TheBaaaaaD <62421223+TheBaaaaaD@users.noreply.github.com>
2020-09-22 15:43:50 +0200

    Create tp20200918_2.h

---
+++ tp20200918_2.h
@@ -0,0 +1,13 @@
+#ifndef DEF_TP20200918_2
+#define DEF_TP20200918_2
+
+
+    typedef struct tpInOut tpIO;
+    struct tpInOut
+    {
+       signed short int sum_In;
+       signed short int sum_Out;
+    };
+
+
+#endif
